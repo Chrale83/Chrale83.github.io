1abe11aa8cee594a90cc17af73e994fb6a2292e6
Chrale83 <86562475+Chrale83@users.noreply.github.com>
2025-02-12 15:20:46 +0100

    update logic for price calculation

--- script.js
+++ script.js
@@ -8,10 +8,16 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 let countPeople = 1;
+const AirPlanePackagePrice = 1250;
+const coffePrice = 10;
+const parachutePrice = 10;
+const insurancePrice = 10;
+
+let totalPackagePrice = 0;
 
 document.querySelectorAll(".dropdown-item").forEach((item) => {
   item.addEventListener("click", function () {
-    countPeople = this.getAttribute("data-value");
+    countPeople = parseInt(this.getAttribute("data-value"));
     console.log(countPeople);
 
     SetCoffePrice();
@@ -20,23 +26,50 @@ document.querySelectorAll(".dropdown-item").forEach((item) => {
   });
 });
 
-function SetCoffePrice() {
-  let coffePrice = 50;
+let isExtraParachuteChecked = document.getElementById("CheckedExtraParachute");
+let isCoffeChecked = document.getElementById("checkedCoffe");
+let isInsuranceChecked = document.getElementById("checkedInsurance");
+let totalPackackePriceElement = document.getElementById("totalPackagePrice");
 
+isExtraParachuteChecked.addEventListener("change", function () {
+  if (isExtraParachuteChecked.checked) {
+    totalPackagePrice += parachutePrice * countPeople;
+  } else {
+    totalPackagePrice -= parachutePrice * countPeople;
+  }
+
+  totalPackackePriceElement.textContent = totalPackagePrice + " kr";
+});
+
+isCoffeChecked.addEventListener("change", function () {
+  if (isCoffeChecked.checked) {
+    totalPackagePrice += coffePrice * countPeople;
+  } else {
+    totalPackagePrice -= coffePrice * countPeople;
+  }
+  totalPackackePriceElement.textContent = totalPackagePrice + " kr";
+});
+
+isInsuranceChecked.addEventListener("change", function () {
+  if (isInsuranceChecked.checked) {
+    totalPackagePrice += insurancePrice * countPeople;
+  } else {
+    totalPackagePrice -= insurancePrice * countPeople;
+  }
+  totalPackackePriceElement.textContent = totalPackagePrice + " kr";
+});
+
+function SetCoffePrice() {
   let coffePriceElement = document.getElementById("coffePrice");
   coffePriceElement.textContent = coffePrice * countPeople;
 }
 
 function SetExtraParachutePrice() {
-  let parachutePrice = 850;
-
   let parachutePriceElement = document.getElementById("parachutePrice");
   parachutePriceElement.textContent = parachutePrice * countPeople;
 }
 
 function SetLifeunsurancePrice() {
-  let insurancePrice = 1450;
-
   let insurancePriceElement = document.getElementById("insurancePrice");
   insurancePriceElement.textContent = insurancePrice * countPeople;
 }
