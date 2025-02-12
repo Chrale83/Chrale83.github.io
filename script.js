f850bc9d07d41b26dc55cce158157b9a1d9e3418
Chrale83 <86562475+Chrale83@users.noreply.github.com>
2025-02-12 13:53:49 +0100

    update products.html

--- script.js
+++ script.js
@@ -7,9 +7,36 @@ document.addEventListener("DOMContentLoaded", function () {
     .catch((error) => console.error("Error loading navbar:", error));
 });
 
+let countPeople = 1;
+
 document.querySelectorAll(".dropdown-item").forEach((item) => {
   item.addEventListener("click", function () {
-    let countPeople = this.getAttribute("data-value");
+    countPeople = this.getAttribute("data-value");
     console.log(countPeople);
+
+    SetCoffePrice();
+    SetLifeunsurancePrice();
+    SetExtraParachutePrice();
   });
 });
+
+function SetCoffePrice() {
+  let coffePrice = 50;
+
+  let coffePriceElement = document.getElementById("coffePrice");
+  coffePriceElement.textContent = coffePrice * countPeople;
+}
+
+function SetExtraParachutePrice() {
+  let parachutePrice = 850;
+
+  let parachutePriceElement = document.getElementById("parachutePrice");
+  parachutePriceElement.textContent = parachutePrice * countPeople;
+}
+
+function SetLifeunsurancePrice() {
+  let insurancePrice = 1450;
+
+  let insurancePriceElement = document.getElementById("insurancePrice");
+  insurancePriceElement.textContent = insurancePrice * countPeople;
+}
