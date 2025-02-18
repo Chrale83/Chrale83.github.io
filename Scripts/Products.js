174225fd3b9f3f5bfab83581c3c42591f1846374
Chrale83 <86562475+Chrale83@users.noreply.github.com>
2025-02-18 14:10:58 +0100

    Update Product logic - Refactoring

--- Scripts/Products.js
+++ Scripts/Products.js
@@ -1,95 +1,48 @@
-const AirPlanePackagePrice = 1000;
+const AirPlanePackagePrice = 12500;
 const coffePrice = 10;
-const parachutePrice = 10;
-const insurancePrice = 10;
-let countPeople = null;
+const parachutePrice = 20;
+const insurancePrice = 30;
 
+let countPeople = null;
 let totalPackagePrice = 0;
 
-let isExtraParachuteChecked = document.getElementById("CheckedExtraParachute");
-let isCoffeChecked = document.getElementById("checkedCoffe");
-let isInsuranceChecked = document.getElementById("checkedInsurance");
-let totalPackagePriceElement = document.getElementById("totalPackagePrice");
 let cancelAirPackage = document.getElementById("cancelAirPackage");
 let selectAirPackage = document.getElementById("selectAirPackage");
+let totalPackagePriceElement = document.getElementById("totalPackagePrice");
 
-selectAirPackage.addEventListener("click", function () {
-  totalPackagePriceElement.textContent = totalPackagePrice;
+document.querySelectorAll(".dropdown-item").forEach((item) => {
+  item.addEventListener("click", function () {
+    countPeople = parseInt(this.getAttribute("data-value"));
+    ChangePassangerQuantity(countPeople);
+    console.log(countPeople);
+
+    document.getElementById("selectAirPackage").disabled = false;
+
+    totalPackagePrice = AirPlanePackagePrice * countPeople;
+    totalPackagePriceElement.textContent = totalPackagePrice;
+  });
 });
 
 cancelAirPackage.addEventListener("click", function () {
   clearInputs();
 });
 
-document.querySelectorAll(".dropdown-item").forEach((item) => {
-  item.addEventListener("click", function () {
-    countPeople = parseInt(this.getAttribute("data-value"));
-    ChangePassangerAmount(countPeople);
-    console.log(countPeople);
-
-    document.getElementById("selectAirPackage").disabled = false;
-
-    SetCoffePrice();
-    SetLifeunsurancePrice();
-    SetExtraParachutePrice();
-    totalPackagePrice = AirPlanePackagePrice * countPeople;
-  });
+selectAirPackage.addEventListener("click", () => {
+  totalPackagePrice += AirPlanePackagePrice * countPeople;
 });
 
-function ChangePassangerAmount(chosenAmountPeople) {
+function ChangePassangerQuantity(selectedQuantityPeople) {
   document.getElementById("selectedAmountPerson").textContent =
-    chosenAmountPeople;
+    selectedQuantityPeople;
 }
 
+// Kod inuti modalen
+
 function clearInputs() {
-  document.getElementById("selectAirPackage").disabled = true;
-  ChangePassangerAmount(null);
   countPeople = null;
-  totalPackagePrice = AirPlanePackagePrice;
+  totalPackagePrice = 0;
   isExtraParachuteChecked.checked = false;
   isCoffeChecked.checked = false;
   isInsuranceChecked.checked = false;
-}
-
-isExtraParachuteChecked.addEventListener("change", function () {
-  if (isExtraParachuteChecked.checked) {
-    totalPackagePrice += parachutePrice * countPeople;
-  } else {
-    totalPackagePrice -= parachutePrice * countPeople;
-  }
-
-  totalPackagePriceElement.textContent = totalPackagePrice;
-});
-
-isCoffeChecked.addEventListener("change", function () {
-  if (isCoffeChecked.checked) {
-    totalPackagePrice += coffePrice * countPeople;
-  } else {
-    totalPackagePrice -= coffePrice * countPeople;
-  }
-  totalPackagePriceElement.textContent = totalPackagePrice + " kr";
-});
-
-isInsuranceChecked.addEventListener("change", function () {
-  if (isInsuranceChecked.checked) {
-    totalPackagePrice += insurancePrice * countPeople;
-  } else {
-    totalPackagePrice -= insurancePrice * countPeople;
-  }
-  totalPackagePriceElement.textContent = totalPackagePrice + " kr";
-});
-
-function SetCoffePrice() {
-  let coffePriceElement = document.getElementById("coffePrice");
-  coffePriceElement.textContent = coffePrice * countPeople;
-}
-
-function SetExtraParachutePrice() {
-  let parachutePriceElement = document.getElementById("parachutePrice");
-  parachutePriceElement.textContent = parachutePrice * countPeople;
-}
-
-function SetLifeunsurancePrice() {
-  let insurancePriceElement = document.getElementById("insurancePrice");
-  insurancePriceElement.textContent = insurancePrice * countPeople;
+  document.getElementById("selectAirPackage").disabled = false;
 }
